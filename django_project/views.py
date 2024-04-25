bda7c42e856aa5f6b1b7152353d84d941409f2ce
onsaseinnocent <32049291-onsaseinnocent@users.noreply.replit.com>
2024-04-25 06:59:25 +0000

    added facts

--- django_project/views.py
+++ django_project/views.py
@@ -2,13 +2,28 @@ from django.shortcuts import render
 import requests
 
 def index(request):
+    # GitHub events
     r1 = requests.get('https://api.github.com/events')
     data = r1.json()
     events = data[0]['repo'] if data else "No events found" 
-    r2= requests.get('https://boredapi.com/api/activity')
-    data= r2.json()
+
+    # Bored API activity
+    r2 = requests.get('https://boredapi.com/api/activity')
+    data = r2.json()
     activity = data['activity']
-    r3= requests.get('https://dog.ceo/api/breeds/image/random')
-    res3= r3.json()
-    dog= res3['message']
-    return render(request, 'index.html', {'event': events, 'activity' : activity,'dog':dog})
+
+    # Dog API image
+    r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+    res3 = r3.json()
+    dog = res3['message']
+
+    # Random Quote
+    r4 = requests.get('https://api.quotable.io/random')
+    quote_data = r4.json()
+    quote = f'"{quote_data["content"]}" - {quote_data["author"]}' if quote_data else "No quote found"
+
+    # Random fact
+    r5 = requests.get('http://numbersapi.com/random/trivia')
+    fact = r5.text if r5.status_code == 200 else "No fact available"
+
+    return render(request, 'index.html', {'event': events, 'activity': activity, 'dog': dog, 'quote': quote, 'fact': fact})
